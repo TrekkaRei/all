9646be9969e561df1456785b00d3f8283707fd6c
trekka <144759737+TrekkaRei@users.noreply.github.com>
2024-05-28 00:13:09 +0200

    Update all.sh

--- all.sh
+++ all.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-git clone https://github.com/TrekkaRei/wallpaper.git
-git clone https://github.com/TrekkaRei/cinnamon_glitch.git
-git clone https://github.com/TrekkaRei/cinnamon_trekka.git
-git clone https://github.com/TrekkaRei/vimrc.git
-git clone https://github.com/TrekkaRei/vifm.git
-git clone https://github.com/TrekkaRei/openrgb.git
-git clone https://github.com/TrekkaRei/flameshot.git
-git clone https://github.com/TrekkaRei/firefox.git
-git clone https://github.com/TrekkaRei/gtk-3.0.git
+git clone https://github.com/TrekkaRei/wallpaper
+git clone https://github.com/TrekkaRei/cinnamon_glitch
+git clone https://github.com/TrekkaRei/cinnamon_trekka
+git clone https://github.com/TrekkaRei/vimrc
+git clone https://github.com/TrekkaRei/vifm
+git clone https://github.com/TrekkaRei/openrgb
+git clone https://github.com/TrekkaRei/flameshot
+git clone https://github.com/TrekkaRei/firefox
+git clone https://github.com/TrekkaRei/gtk-3.0
+git clone https://github.com/TrekkaRei/theme
