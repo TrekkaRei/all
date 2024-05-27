c435fb86f23d02db814ee417d2bcffbfbdbedafb
trekka <144759737+TrekkaRei@users.noreply.github.com>
2024-05-27 14:05:02 +0200

    Add files via upload

---
+++ all.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git clone https://github.com/TrekkaRei/wallpaper.git
+git clone https://github.com/TrekkaRei/cinnamon_glitch.git
+git clone https://github.com/TrekkaRei/cinnamon_trekka.git
+git clone https://github.com/TrekkaRei/vimrc.git
+git clone https://github.com/TrekkaRei/vifm.git
+git clone https://github.com/TrekkaRei/openrgb.git
+git clone https://github.com/TrekkaRei/flameshot.git
+git clone https://github.com/TrekkaRei/firefox.git
+git clone https://github.com/TrekkaRei/gtk-3.0.git
