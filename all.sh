92096194a9ea3e52e43d8e72e73207c7cb44a7de
TrekkaRei <TrekkaRei@gmail.com>
2024-05-30 13:33:40 +0200

    removed deleted repos

--- all.sh
+++ all.sh
@@ -4,11 +4,9 @@ git clone https://github.com/TrekkaRei/cinnamenu
 git clone https://github.com/TrekkaRei/bashrc
 git clone https://github.com/TrekkaRei/wallpaper
 git clone https://github.com/TrekkaRei/cinnamon_glitch
-git clone https://github.com/TrekkaRei/cinnamon_trekka
 git clone https://github.com/TrekkaRei/vimrc
 git clone https://github.com/TrekkaRei/vifm
 git clone https://github.com/TrekkaRei/openrgb
 git clone https://github.com/TrekkaRei/flameshot
 git clone https://github.com/TrekkaRei/firefox
-git clone https://github.com/TrekkaRei/gtk-3.0
 git clone https://github.com/TrekkaRei/theme
